28c4f0083ea86e5021e3689d0dc8cbfef3f33f83
Kaique Almeida <kaique.tubarao2017@gmail.com>
2025-05-27 09:25:00 -0300

    Add CRUD with backend

---
+++ Etapa2/app-react/App4.js
@@ -0,0 +1,34 @@
+import React, { useState } from "react"; 
+import { StyleSheet, View, Button } from 'react-native';
+import ScrollViewExample from './components/ScrollViewExample'
+
+export default function App() {
+
+  const baseUrl = 'http://10.81.205.17:3000';
+
+  const getItems = async () => {
+    const response = await fetch(`${baseUrl}/items`)
+    console.log(response.json());
+  }
+  
+  return (
+    <View style={styles.container}>
+      <Button onPress={getItems} title='Buscar'>
+        buscar dados  
+      </Button>    
+      <ScrollViewExample />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: "column",
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    backgroundColor: 'gray',
+    height: 600,
+    marginTop: 150,
+
+  },
+});
