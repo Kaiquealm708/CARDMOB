0c23e6c2ac05d61a6500afa2db7492c309bf1fa5
Kaique <kaique.tubarao2017@gmail.com>
2025-10-15 10:35:44 -0300

    Tela básica do perfil

--- Etapa3/loja-react-app/src/screens/auth/ProfileScreen.tsx
+++ Etapa3/loja-react-app/src/screens/auth/ProfileScreen.tsx
@@ -1,11 +1,31 @@
-import React from "react";
-import { View, Text, Button, StyleSheet } from "react-native";
-import { useTheme } from "../../contexts/ThemeContext"; // Nova
+import React, { useEffect, useState } from "react"; // modificado
+import { View, Text, Button, StyleSheet, Image } from "react-native"; // modificado
+
+import { useTheme } from "../../contexts/ThemeContext";
 import { useAuth } from "../../contexts/AuthContext";
+
+import { requestProfileById } from "../../services/profileService"
 
 function ProfileScreen({ navigation }: any) {
   const { theme, toggleTheme } = useTheme();
   const { logout } = useAuth();
+  const [user, setUser] = useState({}); // novo
+
+  // novo
+  useEffect(() => {
+    const fetchProfile = async () => {
+      try {
+        const user = await requestProfileById(1);
+        console.log(user);
+        setUser(user);
+        console.log("Carregou o usuário!");
+      } catch (error) {
+        console.error("Erro ao carregar o perfil do usuário:", error);
+      }
+    };
+    fetchProfile();
+  }, []);
+
   return (
     <View
       style={[styles.container, { backgroundColor: theme.colors.background }]}
@@ -15,20 +35,37 @@ function ProfileScreen({ navigation }: any) {
       >
         Profile Screen
       </Text>
-      <Button title="Alternar tema" color={theme.colors.primary} onPress={toggleTheme}/>
-      <Button title="Ir para Detalhes" onPress={ () => navigation.navigate("Details")}/>
-      <Button title="Sair" onPress={logout}/>
+      <View>
+        <Image source={{ uri: user.image }} style={styles.image} />
+      </View>
+      <Text style={styles.text}>{user.name}</Text>
+      <Text style={styles.text}>{user.email}</Text>
+
+      <Button
+        title="Alternar Tema"
+        color={theme.colors.primary}
+        onPress={toggleTheme}
+      />
+      <Button
+        title="Ir para Detalhes"
+        onPress={() => navigation.navigate("Details")}
+      />
+      <Button title="Sair" onPress={logout} />
     </View>
   );
 }
-
 export default ProfileScreen;
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
     justifyContent: "center",
     alignItems: "center",
   },
+  image: {
+    // novo
+    height: 100,
+    width: 100,
+  },
+  text: { fontSize: 14 }, // novo
 });
